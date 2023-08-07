d73b0984ec4782ba1c25f31c445697ff95f09339
Blackberry <david2631@gmail.com>
2023-08-07 14:04:32 -0400

    created arrays for various character sets

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,11 @@
+// Sets of arrays that contain different types of characters that can be added to password generation
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = upperCase.map(element => {
+  return element.toLowerCase();
+});
+var num = ["0","1","2","3","4","5","6","7","8","9"];
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?', '`', '~']
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
